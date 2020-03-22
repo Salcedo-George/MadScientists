0431c053a8aa814c4e1e03f57ff32aacadee4c4a
NSClocal <nsclocal@NSClocals-MacBook-Pro.local>
2020-03-22 16:43:54 -0700

    added thelabratory app.js VAR  changes

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about')
+var aboutRouter = require('./routes/about');
+var thelabratory = require('./routes/thelabratory');
 var app = express();
 
 // view engine setup
