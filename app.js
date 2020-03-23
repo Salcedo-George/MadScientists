acae7550a1c6057377d77b9c7ff43f290702d403
NSClocal <nsclocal@NSClocals-MacBook-Pro.local>
2020-03-22 17:35:24 -0700

    removed typos from app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
-var thelabratoryRouter = require('./routes/thelabratory');git status
+var thelabratoryRouter = require('./routes/thelabratory');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
-app.use('/thelabratory', thelabratoryRouter):
+app.use('/thelabratory', thelabratoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
